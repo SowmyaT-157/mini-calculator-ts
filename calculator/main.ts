4ff9a089756d7e66d9d329013c97fdfaf3ca0cf9
sowmya <sowmya.t@everest.engineering>
2025-05-31 13:57:04 +0530

    Add addition functionality to calculator

--- calculator/main.ts
+++ calculator/main.ts
@@ -1,1 +1,15 @@
 console.log("mini calculator......")
+
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const num1:number = Number(prompt("enter a first number:"))
+const num2:number = Number(prompt("enter a second number:"))
+
+if (isNaN(num1) || isNaN(num2)){
+    console.log("Invalid input")
+}
+else{
+   const sum:number = num1 + num2
+   console.log(`The sum is ${sum}`)
+}
